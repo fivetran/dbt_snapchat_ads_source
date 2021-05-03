fedc81dac3e41750875e96867ba92af6e2733e16
Dylan Baker <dylan@dbanalytics.co>
2021-05-03 17:47:24 -0400

    most recent record for url tags

--- models/stg_snapchat__creative_url_tag_history.sql
+++ models/stg_snapchat__creative_url_tag_history.sql
@@ -24,8 +24,18 @@ final as (
     select  
         creative_id,
         key as param_key,
-        value as param_value
+        value as param_value,
+        updated_at
     from fields
+),
+
+most_recent as (
+
+    select 
+        *,
+        row_number() over (partition by creative_id, param_key order by updated_at desc) =1 as is_most_recent_record
+    from final
+
 )
 
-select * from final
+select * from most_recent
