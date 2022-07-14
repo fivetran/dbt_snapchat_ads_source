4b894cc3dafc1f399feca46c5ba2c2722a6e092d
fivetran-reneeli <renee.li@fivetran.com>
2022-07-14 15:38:14 -0400

    most recent use updated at not fivetran synced

--- models/stg_snapchat_ads__creative_history.sql
+++ models/stg_snapchat_ads__creative_history.sql
@@ -33,7 +33,7 @@ most_recent as (
 
     select 
         *,
-        row_number() over (partition by creative_id order by _fivetran_synced desc) =1 as is_most_recent_record
+        row_number() over (partition by creative_id order by updated_at desc) =1 as is_most_recent_record
     from final
 )
 
