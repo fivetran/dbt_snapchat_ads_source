1e9b8b0c975b66093b78440922519bc97c1da392
fivetran-reneeli <renee.li@fivetran.com>
2022-07-27 20:42:58 +0200

    accidentally deleted name column

--- macros/get_ad_account_history_columns.sql
+++ macros/get_ad_account_history_columns.sql
@@ -6,6 +6,7 @@
     {"name": "created_at", "datatype": dbt_utils.type_timestamp()},
     {"name": "currency", "datatype": dbt_utils.type_string()},
     {"name": "id", "datatype": dbt_utils.type_string()},
+    {"name": "name", "datatype": dbt_utils.type_string()},
     {"name": "timezone", "datatype": dbt_utils.type_string()},
     {"name": "type", "datatype": dbt_utils.type_string()},
     {"name": "updated_at", "datatype": dbt_utils.type_timestamp()}
