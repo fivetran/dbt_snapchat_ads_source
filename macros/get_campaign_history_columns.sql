aff613f726b8f371ec93c34d2594b70831391da7
Renee Li <renee.li@fivetran.com>
2025-04-22 13:19:10 -0400

    add new campaign history fields

--- macros/get_campaign_history_columns.sql
+++ macros/get_campaign_history_columns.sql
@@ -6,7 +6,13 @@
     {"name": "created_at", "datatype": dbt.type_timestamp()},
     {"name": "id", "datatype": dbt.type_string()},
     {"name": "name", "datatype": dbt.type_string()},
-    {"name": "updated_at", "datatype": dbt.type_timestamp()}
+    {"name": "updated_at", "datatype": dbt.type_timestamp()},
+    {"name": "daily_budget_micro", "datatype": dbt.type_int()},
+    {"name": "start_time", "datatype": dbt.type_timestamp()},
+    {"name": "end_time", "datatype": dbt.type_timestamp()},
+    {"name": "lifetime_spend_cap_micro", "datatype": dbt.type_int()},
+    {"name": "status", "datatype": dbt.type_string()},
+    {"name": "objective", "datatype": dbt.type_string()}
 ] %}
 
 {{ return(columns) }}
--- models/stg_snapchat_ads__campaign_history.sql
+++ models/stg_snapchat_ads__campaign_history.sql
@@ -4,6 +4,7 @@ with base as (
 
     select * 
     from {{ ref('stg_snapchat_ads__campaign_history_tmp') }}
+
 ),
 
 fields as (
@@ -15,13 +16,14 @@ fields as (
                 staging_columns=get_campaign_history_columns()
             )
         }}
-    
+
         {{ fivetran_utils.source_relation(
             union_schema_variable='snapchat_ads_union_schemas', 
             union_database_variable='snapchat_ads_union_databases') 
         }}
 
     from base
+
 ),
 
 final as (
@@ -30,12 +32,19 @@ final as (
         source_relation,
         id as campaign_id,
         ad_account_id,
-        cast (created_at as {{ dbt.type_timestamp() }}) as created_at,
+        cast(created_at as {{ dbt.type_timestamp() }}) as created_at,
         name as campaign_name,
-        cast (_fivetran_synced as {{ dbt.type_timestamp() }}) as _fivetran_synced,
-        cast (updated_at as {{ dbt.type_timestamp() }}) as updated_at,
+        cast(_fivetran_synced as {{ dbt.type_timestamp() }}) as _fivetran_synced,
+        cast(updated_at as {{ dbt.type_timestamp() }}) as updated_at,
+        cast(daily_budget_micro as {{ dbt.type_int() }}) as daily_budget_micro,
+        cast(start_time as {{ dbt.type_timestamp() }}) as start_time,
+        cast(end_time as {{ dbt.type_timestamp() }}) as end_time,
+        cast(lifetime_spend_cap_micro as {{ dbt.type_int() }}) as lifetime_spend_cap_micro,
+        status,
+        objective,
         row_number() over (partition by source_relation, id order by _fivetran_synced desc) = 1 as is_most_recent_record
     from fields
+
 )
 
 select * 
